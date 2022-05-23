7614350ee0d41d45c8e9777b2a0bd0475c3f71a8
mark l chaves <mark@caughtmyeye.cc>
2022-05-23 13:17:59 +0800

    First commit

---
+++ jest-lyrics/README.md
@@ -0,0 +1,10 @@
+# Jest Lyrics
+
+## Goal
+
+Make the tests work.
+
+## Getting Started
+
+1. Install Jest by running `npm install jest --D`
+2. Run the tests by running `npm run test`
---
+++ jest-lyrics/lyrics.test.js
@@ -0,0 +1,25 @@
+const Lyrics = require("./lyrics");
+
+describe("Tests for single lines.", () => {
+  test("First line", () => {
+    const lineText = `I saw her today at the reception.`;
+    expect(new Lyrics().line(0)).toBe(lineText);
+  });
+
+  test.skip("Second line", () => {
+    const lineText = `A glass of wine in her hand.`;
+    expect(new Lyrics().line(1)).toBe(lineText);
+  });
+
+  test.skip("Third line", () => {
+    const lineText = `I knew she would meet her connection.`;
+    expect(new Lyrics().line(2)).toBe(lineText);
+  });
+});
+
+describe("Tests for a range of lines.", () => {
+  test.skip("The first three lines", () => {
+    const lineText = `I saw her today at the reception. A glass of wine in her hand. I knew she would meet her connection.`;
+    expect(new Lyrics().lines(0, 2)).toBe(lineText);
+  });
+});
